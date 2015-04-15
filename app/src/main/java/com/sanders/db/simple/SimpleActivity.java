fb29b284abd93071abb98776803d4d89ac6c05f3
Mr. Sanders <1123033157@qq.com>
2015-04-15 14:52:48 +0800

    增加注释及扩展2个方法

--- app/src/main/java/com/sanders/db/simple/SimpleActivity.java
+++ app/src/main/java/com/sanders/db/simple/SimpleActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.sanders.db.DBProxy;
 import com.sanders.db.Table1;
@@ -12,6 +13,7 @@ import com.sanders.db.Table_2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by sanders on 15/3/27.
@@ -24,35 +26,16 @@ public class SimpleActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple);
-//        db = new ProxyDB(new SimpleOpenHelper(this));
-//        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.layout);
-//        for (int i = 0; i < linearLayout.getChildCount(); i++) {
-//            linearLayout.getChildAt(i).setOnClickListener(this);
-//        }
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.layout);
+        for (int i = 0; i < linearLayout.getChildCount(); i++) {
+            linearLayout.getChildAt(i).setOnClickListener(this);
+        }
 
         db = new DBProxy.DBBuilder()
                 .builderDbName("test")
                 .builderDbVersion(1)
-                .builderTable(Table1.class).builderTable(Table_2.class).builderTable(TableBean.class)
+                .builderTable(UserTable.class).builderTable(Table1.class).builderTable(Table_2.class).builderTable(TableBean.class)
                 .build(this);
-        List<Table1> list = db.queryList(Table1.class, null, null);
-        for(Table1 t : list){
-            Log.e("ESA", t.getKeyId()+"");
-        }
-//        db.queryList(Table1.class, null, null);
-//
-//        try {
-//            String path = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_UNINSTALLED_PACKAGES).sourceDir;
-//            DexFile dexfile = new DexFile(path);
-//            Enumeration<String> entries = dexfile.entries();
-//            while (entries.hasMoreElements()) {
-//                String name = entries.nextElement();
-////                if(name.startsWith("com.sanders.db") && !name.contains("$"))
-//                    Log.e("ESA", name);
-//            }
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
     }
 
     long id = 1;
@@ -62,56 +45,44 @@ public class SimpleActivity extends Activity implements View.OnClickListener {
         int viewId = v.getId();
         switch (viewId) {
             case R.id.btn_insert:
-                long startTime = System.currentTimeMillis();
                 UserTable table = new UserTable("username", "password", 100);
                 db.insert(table);
-                Log.e("ESA", String.valueOf(System.currentTimeMillis() - startTime));
-                Log.e("ESA", "KEY_ID="+table.getKeyId());
                 break;
             case R.id.btn_insert_list:
                 List<UserTable> list = new ArrayList<UserTable>();
                 for (int i = 1; i <= 10; i++) {
                     list.add(new UserTable("username" + i, "password" + i, i));
                 }
-                startTime = System.currentTimeMillis();
                 db.insert(list);
-                Log.e("ESA", String.valueOf(System.currentTimeMillis() - startTime));
-                for(int i=0; i<10; i++){
+                for (int i = 0; i < 10; i++) {
                     Log.e("ESA", "KEY_ID=" + list.get(i).getKeyId());
                 }
                 break;
             case R.id.btn_update:
                 table = new UserTable("update", "update", 100);
                 table.setKeyId(id);
-                startTime = System.currentTimeMillis();
                 db.update(table);
-                Log.e("ESA", String.valueOf(System.currentTimeMillis() - startTime));
                 id++;
                 break;
             case R.id.btn_update_list:
                 list = new ArrayList<UserTable>();
-                for(int i=11; i<=20; i++){
+                for (int i = 11; i <= 20; i++) {
                     table = new UserTable("UPDATE", "UPDATE", 0);
                     table.setKeyId(i);
                     list.add(table);
                 }
-                startTime = System.currentTimeMillis();
                 db.update(list);
-                Log.e("ESA", String.valueOf(System.currentTimeMillis() - startTime));
                 break;
             case R.id.btn_delete:
-                startTime = System.currentTimeMillis();
                 db.delete(UserTable.class, "age=?", String.valueOf(12));
-                Log.e("ESA", String.valueOf(System.currentTimeMillis() - startTime));
                 break;
             case R.id.btn_query:
-                db.query(UserTable.class, 1);
+                Map<String, Object> map = db.query("SELECT * FROM user_table");
+                Log.e("ESA", map.toString());
                 break;
             case R.id.btn_query_list:
-                startTime = System.currentTimeMillis();
                 list = db.queryList(UserTable.class, "age=?", String.valueOf(100));
-                Log.e("ESA", String.valueOf(System.currentTimeMillis() - startTime));
-                for(UserTable t : list){
+                for (UserTable t : list) {
                     Log.e("ESA", "KEY_ID=" + t.getKeyId());
                 }
                 break;
--- library/src/main/java/com/sanders/db/DBProxy.java
+++ library/src/main/java/com/sanders/db/DBProxy.java
@@ -6,6 +6,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -15,39 +16,89 @@ import java.util.Set;
 
 /**
  * Created by sanders on 15/4/4.
+ * 数据库操作类
  */
 public class DBProxy {
 
+    /**
+     * 用于缓存实体类Class和实体类详情
+     */
     public Map<Class, ClassInfo> classInfoMap = new HashMap<Class, ClassInfo>();
+    /**
+     * SQLiteOpenHelper实现类
+     */
     private SQLiteOpenHelper helper;
+    /**
+     * 数据库操作计数，防止异常关闭问题
+     */
     private int closeIndex = 0;
 
+    /**
+     * 构建数据库操作类
+     */
     public static class DBBuilder {
+        /**
+         * 数据库名称
+         */
         private String dbName;
+        /**
+         * 数据库版本
+         */
         private int dbVersion;
+        /**
+         * 数据库升级接口
+         */
         private DefaultSQLiteOpenHelper.OnDBUpgrade upgrade;
+        /**
+         * 自动建表Class集合
+         */
         private Set<Class> classes = new HashSet<Class>();
 
+        /**
+         * 设置数据库名称
+         * @param dbName
+         * @return
+         */
         public DBBuilder builderDbName(String dbName) {
             this.dbName = dbName;
             return this;
         }
 
+        /**
+         * 设置数据库版本号
+         * @param dbVersion
+         * @return
+         */
         public DBBuilder builderDbVersion(int dbVersion) {
             this.dbVersion = dbVersion;
             return this;
         }
 
+        /**
+         * 设置对应的实体类Class并会自动建表
+         * @param clazz
+         * @return
+         */
         public DBBuilder builderTable(Class clazz) {
             this.classes.add(clazz);
             return this;
         }
 
-        public DBBuilder setOnDBUpgrade(DefaultSQLiteOpenHelper.OnDBUpgrade upgrade){
+        /**
+         * 设置数据库升级操作接口实现类
+         * @param upgrade
+         * @return
+         */
+        public DBBuilder setOnDBUpgrade(DefaultSQLiteOpenHelper.OnDBUpgrade upgrade) {
             this.upgrade = upgrade;
             return this;
         }
 
+        /**
+         * build一个数据库操作类
+         * @param context
+         * @return
+         */
         public DBProxy build(Context context) {
             DBProxy proxy = new DBProxy();
             DefaultSQLiteOpenHelper helper = new DefaultSQLiteOpenHelper(context, dbName, dbVersion, classes, upgrade);
@@ -57,18 +108,32 @@ public class DBProxy {
         }
     }
 
-    private DBProxy(){
+    private DBProxy() {
 
     }
 
-    private void setSQLiteOpenHelper(SQLiteOpenHelper helper){
+    /**
+     * 设置实现SQLiteOpenHelper类的实现
+     * @param helper
+     */
+    private void setSQLiteOpenHelper(SQLiteOpenHelper helper) {
         this.helper = helper;
     }
 
+    /**
+     * 构建数据库操作类
+     * @param helper
+     */
     public DBProxy(SQLiteOpenHelper helper) {
         this.helper = helper;
     }
 
+    /**
+     * 获取一个实体类Class的详细信息并缓存
+     * @param t
+     * @param <T>
+     * @return
+     */
     private synchronized <T extends IDColumn> ClassInfo getClassInfo(T t) {
         ClassInfo classInfo = classInfoMap.get(t.getClass());
         if (classInfo == null) {
@@ -77,6 +142,12 @@ public class DBProxy {
         return classInfo;
     }
 
+    /**
+     * 获取一个实体类Class的详细信息并缓存
+     * @param clazz
+     * @param <T>
+     * @return
+     */
     public synchronized <T extends IDColumn> ClassInfo getClassInfo(Class<T> clazz) {
         ClassInfo classInfo = classInfoMap.get(clazz);
         if (classInfo == null) {
@@ -85,6 +156,12 @@ public class DBProxy {
         return classInfo;
     }
 
+    /**
+     * 插入对应实体到数据库
+     * @param t
+     * @param <T>
+     * @return
+     */
     public synchronized <T extends IDColumn> long insert(T t) {
         if (t == null) {
             return -1;
@@ -105,6 +182,11 @@ public class DBProxy {
         return -1;
     }
 
+    /**
+     * 批量插入对应实体类到数据库。建议集合不要太大，这是一次性事务
+     * @param list
+     * @param <T>
+     */
     public synchronized <T extends IDColumn> void insert(List<T> list) {
         if (isEmpty(list)) {
             return;
@@ -124,6 +206,14 @@ public class DBProxy {
         close(database);
     }
 
+    /**
+     * 更具条件更新实体到数据库
+     * @param t
+     * @param where
+     * @param args
+     * @param <T>
+     * @return
+     */
     public synchronized <T extends IDColumn> int update(T t, String where, String... args) {
         if (t == null) {
             throw new NullPointerException("T对象不能为NULL！");
@@ -147,6 +237,12 @@ public class DBProxy {
         return row;
     }
 
+    /**
+     * 更具实体中的主键(_key_id)更新实体到数据库
+     * @param t
+     * @param <T>
+     * @return
+     */
     public synchronized <T extends IDColumn> int update(T t) {
         long keyId;
         if (t == null) {
@@ -157,10 +253,22 @@ public class DBProxy {
         return update(t, IDColumn.KEY_ID + "=" + keyId, null);
     }
 
+    /**
+     * 更具实体中的主键更新实体到数据库
+     * @param t
+     * @param keyId
+     * @param <T>
+     * @return
+     */
     public synchronized <T extends IDColumn> int update(T t, long keyId) {
         return update(t, IDColumn.KEY_ID + "=" + keyId, null);
     }
 
+    /**
+     * 更具集合实体中的主键(_key_id)更新实体到数据库
+     * @param list
+     * @param <T>
+     */
     public synchronized <T extends IDColumn> void update(List<T> list) {
         if (isEmpty(list)) {
             return;
@@ -183,6 +291,11 @@ public class DBProxy {
         close(database);
     }
 
+    /**
+     * 插入或者更新集合
+     * @param list
+     * @param <T>
+     */
     public synchronized <T extends IDColumn> void insertOrUpdate(List<T> list) {
         if (isEmpty(list)) {
             return;
@@ -207,6 +320,10 @@ public class DBProxy {
         close(database);
     }
 
+    /**
+     * 执行原生sql
+     * @param sql
+     */
     public synchronized void execSQL(String... sql) {
         SQLiteDatabase database = getDatabase();
         database.beginTransaction();
@@ -218,6 +335,14 @@ public class DBProxy {
         close(database);
     }
 
+    /**
+     *
+     * 更具条件删除数据库内容
+     * @param clazz
+     * @param where
+     * @param args
+     * @return
+     */
     public synchronized int delete(Class<?> clazz, String where, String... args) {
         String table = ClassInfo.conversionClassNameToTableName(clazz.getName());
         SQLiteDatabase database = getDatabase();
@@ -229,10 +354,24 @@ public class DBProxy {
         return row;
     }
 
+    /**
+     * 根据主键删除数据库内容
+     * @param clazz
+     * @param keyId
+     * @return
+     */
     public synchronized int delete(Class<?> clazz, long keyId) {
         return delete(clazz, IDColumn.KEY_ID + "=" + keyId, null);
     }
 
+    /**
+     * 查询数量
+     * @param clazz
+     * @param where
+     * @param args
+     * @param <T>
+     * @return
+     */
     public <T extends IDColumn> long queryCount(Class<T> clazz, String where, String... args) {
         SQLiteDatabase database = getDatabase();
         ClassInfo<T> classInfo = getClassInfo(clazz);
@@ -252,6 +391,14 @@ public class DBProxy {
         return count;
     }
 
+    /**
+     * 查询主键
+     * @param clazz
+     * @param where
+     * @param args
+     * @param <T>
+     * @return
+     */
     public <T extends IDColumn> long queryKeyId(Class<T> clazz, String where, String... args) {
         if (where == null) {
             throw new NullPointerException("缺少WHERE条件语句！");
@@ -269,6 +416,14 @@ public class DBProxy {
         return id;
     }
 
+    /**
+     * 根据条件查询一条记录到实体
+     * @param clazz
+     * @param where
+     * @param args
+     * @param <T>
+     * @return
+     */
     public <T extends IDColumn> T query(Class<T> clazz, String where, String... args) {
         SQLiteDatabase database = getDatabase();
         ClassInfo<T> classInfo = getClassInfo(clazz);
@@ -279,10 +434,25 @@ public class DBProxy {
         return t;
     }
 
+    /**
+     * 根据主键查询实体
+     * @param clazz
+     * @param keyId
+     * @param <T>
+     * @return
+     */
     public <T extends IDColumn> T query(Class<T> clazz, long keyId) {
         return query(clazz, IDColumn.KEY_ID + "=" + keyId, null);
     }
 
+    /**
+     * 根据sql语句查询实体
+     * @param clazz
+     * @param sql
+     * @param args
+     * @param <T>
+     * @return
+     */
     public <T extends IDColumn> T querySql(Class<T> clazz, String sql, String... args) {
         SQLiteDatabase database = getDatabase();
         Cursor cursor = database.rawQuery(sql, args);
@@ -293,6 +463,14 @@ public class DBProxy {
         return t;
     }
 
+    /**
+     * 根据sql语句查询实体集合
+     * @param clazz
+     * @param sql
+     * @param args
+     * @param <T>
+     * @return
+     */
     public <T extends IDColumn> List<T> querySqlList(Class<T> clazz, String sql, String... args) {
         SQLiteDatabase database = getDatabase();
         Cursor cursor = database.rawQuery(sql, args);
@@ -303,6 +481,18 @@ public class DBProxy {
         return list;
     }
 
+    /**
+     * 更具条件插叙实体集合
+     * @param clazz
+     * @param selection
+     * @param selectionArgs
+     * @param groupBy
+     * @param having
+     * @param orderBy
+     * @param limit
+     * @param <T>
+     * @return
+     */
     public <T extends IDColumn> List<T> queryList(Class<T> clazz, String selection, String[] selectionArgs, String groupBy, String having, String orderBy, String limit) {
         ClassInfo<T> classInfo = getClassInfo(clazz);
         SQLiteDatabase database = getDatabase();
@@ -313,22 +503,108 @@ public class DBProxy {
         return list;
     }
 
+    /**
+     * 分页查询实体集合
+     * @param clazz
+     * @param selection
+     * @param pageNumber
+     * @param pageSize
+     * @param selectionArgs
+     * @param <T>
+     * @return
+     */
     public <T extends IDColumn> List<T> queryList(Class<T> clazz, String selection, int pageNumber, int pageSize, String... selectionArgs) {
         int startPosition = ((pageNumber - 1) * pageSize);
         return queryList(clazz, selection, selectionArgs, null, null, null, startPosition + "," + pageSize);
     }
 
+    /**
+     * 根据条件查询实体集合
+     * @param clazz
+     * @param selection
+     * @param selectionArgs
+     * @param <T>
+     * @return
+     */
     public <T extends IDColumn> List<T> queryList(Class<T> clazz, String selection, String... selectionArgs) {
         return queryList(clazz, selection, selectionArgs, null, null, null, null);
     }
 
+    /**
+     * 查询一条记录到Map
+     * @param sql
+     * @param args
+     * @return
+     */
+    public Map<String, Object> query(String sql, String... args) {
+        SQLiteDatabase database = getDatabase();
+        Cursor cursor = database.rawQuery(sql, args);
+        String[] names = cursor.getColumnNames();
+        Map<String, Object> map = null;
+        if (cursor.moveToNext()) {
+            map = new HashMap<String, Object>();
+            for (String columnName : names) {
+                putMapKeyValue(cursor, columnName, map);
+            }
+        }
+        close(cursor);
+        close(database);
+        return map;
+    }
 
-    public synchronized SQLiteDatabase getDatabase() {
+    /**
+     * 根据sql语句查询map到list
+     * @param sql
+     * @param args
+     * @return
+     */
+    public List<Map<String, Object>> queryList(String sql, String... args){
+        SQLiteDatabase database = getDatabase();
+        Cursor cursor = database.rawQuery(sql, args);
+        String[] names = cursor.getColumnNames();
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        while (cursor.moveToNext()){
+            Map<String, Object> map = new HashMap<String, Object>();
+            for (String columnName : names) {
+                putMapKeyValue(cursor, columnName, map);
+            }
+            list.add(map);
+        }
+        close(cursor);
+        close(database);
+        return list;
+    }
+
+    private void putMapKeyValue(Cursor cursor, String columnName, Map<String, Object> map){
+        int columnIndex = cursor.getColumnIndex(columnName);
+        int type = cursor.getType(columnIndex);
+        switch (type) {
+            case Cursor.FIELD_TYPE_INTEGER:
+                map.put(columnName, cursor.getLong(columnIndex));
+                break;
+            case Cursor.FIELD_TYPE_STRING:
+                map.put(columnName, cursor.getString(columnIndex));
+                break;
+            case Cursor.FIELD_TYPE_FLOAT:
+                map.put(columnName, cursor.getFloat(columnIndex));
+                break;
+            case Cursor.FIELD_TYPE_BLOB:
+                map.put(columnName, cursor.getBlob(columnIndex));
+                break;
+            case Cursor.FIELD_TYPE_NULL:
+                map.put(columnName, cursor.getString(columnIndex));
+                break;
+            default:
+                break;
+        }
+    }
+
+    private synchronized SQLiteDatabase getDatabase() {
         closeIndex++;
         return helper.getWritableDatabase();
     }
 
-    public synchronized void close(SQLiteDatabase database) {
+    private synchronized void close(SQLiteDatabase database) {
         closeIndex--;
         if (closeIndex == 0) {
             if (database != null && database.isOpen()) {
@@ -337,13 +613,13 @@ public class DBProxy {
         }
     }
 
-    public void close(Cursor cursor) {
+    private void close(Cursor cursor) {
         if (cursor != null && !cursor.isClosed()) {
             cursor.close();
         }
     }
 
-    public boolean isEmpty(Collection collection) {
+    private boolean isEmpty(Collection collection) {
         if (collection == null || collection.isEmpty()) {
             return true;
         }
